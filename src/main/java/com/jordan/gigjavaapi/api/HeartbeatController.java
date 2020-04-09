3d8dd01e8fddf33e36447c9bb7316da2d3eb67d6
sammu_97 <sammutlee@gmail.com>
2020-04-09 10:44:13 +0200

    Added HeartbeatController Fixed testScript

---
+++ src/main/java/com/jordan/gigjavaapi/api/HeartbeatController.java
@@ -0,0 +1,15 @@
+package com.jordan.gigjavaapi.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("heartbeatt")
+public class HeartbeatController {
+
+    @GetMapping
+    public void sendHeartbeat(){
+        return;
+    }
+}
