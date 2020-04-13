3f48013712935dff81a35c2479333703b27df237
sammu_97 <sammutlee@gmail.com>
2020-04-13 19:46:34 +0200

    - Switched to MySQL database - Created a docker-compose.yml file to create relevant containers - updated test script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,19 @@
-FROM openjdk:15
-ADD target/gig-java-api.jar gig-java-api.jar
+#
+# Build stage
+#
+FROM maven:3.6.3-jdk-11-slim AS build
+COPY src /home/app/src
+COPY pom.xml /home/app
+RUN mvn -f /home/app/pom.xml clean package
+
+#
+# Package stage
+#
+FROM openjdk
+COPY --from=build /home/app/target/gig-java-api.jar /usr/local/lib/gig-java-api.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "gig-java-api.jar"]
+
+COPY wait-for-it.sh /wait-for-it.sh
+RUN chmod +x /wait-for-it.sh
+
+ENTRYPOINT ["java","-jar","/usr/local/lib/gig-java-api.jar"]
--- testScript.sh
+++ testScript.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
+printf "Initializing Docker Containers...\n"
+docker-compose up -d --build
+printf "All Docker containers are running!\n\n"
 
-printf "Beginning Build...\n"
-mvn package
-printf "Build successfull!\n"
-
-printf "Now building docker image...\n"
-docker build -t gig-java-api .
-docker run -d --rm -p 8080:8080 --name gig-java-api-container gig-java-api
-printf "Docker container is running\n\n"
-
-until [ "$(curl -w '%{response_code}' --no-keepalive -o /dev/null --connect-timeout 1 localhost:8080/heartbeatt)" == "200" ];
+until [ "$(curl -w '%{response_code}' --no-keepalive -o /dev/null --connect-timeout 1 localhost:8080/heartbeat)" == "200" ];
 do echo --- sleeping for 1 second;
 sleep 1;
 done
 
-printf "Web application has started! Begnning Test...\n\n"
+printf "Web application has started! Beginning Test...\n\n"
 
 printf "Adding Account '0000001' with a balance of €20000\n"
-addResponse1=$(curl --header "Content-Type: application/json" --request POST --data '{"balance": "20000","accountNumber": "0000001"}' localhost:8080/api/account/addOrUpdate)
+addResponse1=$(curl -s --header "Content-Type: application/json" --request POST --data '{"balance": "20000","accountNumber": "0000001"}' localhost:8080/api/account/addOrUpdate)
 
 printf "\nAdding Account '0000002' with a balance of €10000\n"
-addRespons2=$(curl --header "Content-Type: application/json" --request POST --data '{"balance": "10000","accountNumber": "0000002"}' localhost:8080/api/account/addOrUpdate)
+addRespons2=$(curl -s --header "Content-Type: application/json" --request POST --data '{"balance": "10000","accountNumber": "0000002"}' localhost:8080/api/account/addOrUpdate)
 
 printf "\nGetting list of accounts within database now...\n"
 curl --request GET localhost:8080/api/account/getAll
 printf "\n"
 
 printf "\nMaking a bank transfer of €10000 from Account '0000002' to Account '0000001'\n"
-curl --header "Content-Type: application/json" \
+curl -s --header "Content-Type: application/json" \
   --request POST \
   --data '{"sender": "0000002","receiver": "0000001","amount": "10000"}' \
   localhost:8080/api/account/bankTransfer
 printf "Transfer completed!\n\n"
 
-printf "Retrieving Balance for Account Number '0000001'"
-balance_0000001=$(curl --request GET localhost:8080/api/account/balanceByAccountNo/0000001)
+printf "Retrieving Balance for Account Number '0000001'\n"
+balance_0000001=$(curl -s --request GET localhost:8080/api/account/balanceByAccountNo/0000001)
 printf "Balance for Account Number '0000001' is €%s\n" $balance_0000001
 
 if [ "$balance_0000001" == 30000.0 ]
@@ -45,8 +39,8 @@ else
     exit 0
 fi
 
-printf "Retrieving Balance for Account Number '0000002'"
-balance_0000002=$(curl --request GET localhost:8080/api/account/balanceByAccountNo/0000002)
+printf "Retrieving Balance for Account Number '0000002'\n"
+balance_0000002=$(curl -s --request GET localhost:8080/api/account/balanceByAccountNo/0000002)
 printf "Balance for Account Number '0000002' is €%s\n" $balance_0000002
 
 if [ "$balance_0000002" == 0.0 ]
@@ -59,6 +53,10 @@ fi
 
 printf "\nTests completed! Stopping container...\n"
 docker stop gig-java-api-container
+docker stop java-api-mysql
+
+docker rm gig-java-api-container
+docker rm java-api-mysql
 
 
 
